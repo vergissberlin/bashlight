9899b0f6249346ae6e50ccedf4187860260fb273
André Lademann <andre.lademann@netresearch.de>
2016-03-29 22:10:18 +0200

    Add more git aliases

--- git-init.sh
+++ git-init.sh
@@ -21,6 +21,7 @@ if [ "$email" != "" ]; then
   git config --global user.email $email
 fi
 
+
 ## aliases
 git config --global alias.br 'branch'
 git config --global alias.ci 'commit -a'
@@ -34,6 +35,14 @@ git config --global alias.sl 'log --color --graph --pretty=format:"%Cred%h%Crese
 git config --global alias.st 'status --ignore-submodules=all'
 git config --global alias.tush 'push --tags'
 git config --global alias.type 'cat-file -t'
+git config --global alias.up = !git fetch origin && git rebase origin/master
+git config --global alias.amend  = !git log -n 1 --pretty=tformat:%s%n%n%b | git commit -F - --amend
+
+git config --global alias.master    = checkout master
+git config --global alias.develop   = checkout develop
+git config --global alias.create-branch = !sh -c 'git push origin HEAD:refs/heads/$1 && git fetch origin && git branch --track $1 origin/$1 && cd . && git checkout $1' -
+git config --global alias.delete-branch = !sh -c 'git push origin :refs/heads/$1 && git branch -D $1' -
+git config --global alias.merge-branch = !git checkout master && git merge @{-1}
 
 ## branch setting
 git config --global branch.autosetuprebase always
