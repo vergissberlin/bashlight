eea633da1f97411c939909b55450d101de5afcb3
Andre Lademann <andre.lademann@netresearch.de>
2018-06-14 11:27:21 +0200

    Added quote of the day

---
+++ src/quotes.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+####################################################################################
+# Bashlight 	: 0.1.8
+# Copyright		: 2018, MIT
+# Author		: André Lademann <vergissberlin@googlemail.com>
+# Repository	: https://github.com/vergissberlin/bashlight
+####################################################################################
+
+curl https://taeglicheszit.at/zitat-api.php?format=full
+echo -e "\n"
